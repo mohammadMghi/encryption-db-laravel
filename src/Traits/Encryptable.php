f09d34348d815c839dcc872be1f6c181f824d82d
Mohammad Moghadasi <mohammadmghig@gmail.com>
2025-11-29 21:08:06 +0330

    Encryptable tait

---
+++ src/Traits/Encryptable.php
@@ -0,0 +1,31 @@
+<?php 
+
+namespace WhiteStarCode\DbCipher\Traits;
+
+use WhiteStarCode\DbCipher\Facades\Encryption;
+
+trait Encryptable
+{
+    protected $encryptable = [];
+
+    public function setAttribute($key,$value)
+    {
+        if (in_array($key , $this->encryptable)) {
+            $value = Encryption::encrypt($value);
+        }
+
+        return parent::setAttribute($key,$value);
+    }
+
+    public function getAttribute($key)
+    {
+        $value = parent::getAttribute($key);
+
+        if (in_array($key , $this->encryptable))
+        {
+            return Encryption::decrypt($value);
+        }
+
+        return $value;
+    }
+}
